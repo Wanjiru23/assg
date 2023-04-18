d1ecf12e3aca4f4172be8172a54a90f7578db8d6
cynthia_wachira <cynthiawachira915@gmail.com>
2023-04-18 07:49:30 +0300

    bank.py

---
+++ variables.py/bank.py
@@ -0,0 +1,10 @@
+greeting=input("enter your greeting here;")
+def mon(greeting):
+ if(greeting=="hello..."):
+  print("$0")
+ elif(greeting[0]=="h"):
+  print("$20")
+ else:
+  print("$100") 
+ 
+mon(greeting)
--- variables.py/variable.py
+++ variables.py/variable.py
@@ -1,1 +1,9 @@
 print("hello world1")
+#variables are names attached to values that can be called later in the code
+e="thanks."
+print(e)
+balance=10
+entries=2
+print(balance*entries)
+#variable names can only start with an underscore or a letter
+#variable names use snake case where the words are separated by an underscore if the variable names has more than one word in it
